574f466d0b065be5619e658e9074d8e4ae9e5ee0
Shashank Mandil <mandil.shashank@gmail.com>
2016-03-06 18:30:10 -0800

    Completed the logout functionality

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  def is_user_loggedin
+    if session[:userlogin_id]
+      return true
+    end
+    false
+  end
 end
--- app/models/common/login.rb
+++ app/models/common/login.rb
@@ -1,10 +1,4 @@
 module Common
   module Login
-    def is_user_loggedin
-      if session[:userlogin_id]
-        return true
-      end
-      false
-    end
   end
 end
