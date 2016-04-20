1dc810ba47a4c45747dec1ce08aeb6af1b51f8c4
Shashank Mandil <mandil.shashank@gmail.com>
2016-04-19 23:43:54 -0700

    Adding the functionality to check for the location presence based on the parameter passed

--- app/controllers/order_controller.rb
+++ app/controllers/order_controller.rb
@@ -5,7 +5,12 @@ class OrderController < ApplicationController
   def index
     @userlogin_id = session[:userlogin_id]
     #Find the locations for this user login id
-    @locations = Location.joins(location_userprofile: {user_profile: :user_login} ).where("user_logins.id = ?", @userlogin_id)
+    if(params.has_key?(:location_id))
+      @location = Location.where("id = " + params[:location_id])
+      @location_present = true
+    else   
+      @location = Location.joins(location_userprofile: {user_profile: :user_login} ).where("user_logins.id = ?", @userlogin_id)
+    end 
   end
 
   #Post controller for the order page
